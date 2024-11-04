88f1fbdd2a10c0f8a3767f6328ad508cb7e93b91
zervakisai <zervakisai@gmail.com>
2024-11-05 00:12:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+// JavaScript για την ανίχνευση της κύλισης και αλλαγή του φόντου
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 100) { // Ενεργοποίηση του νέου φόντου μετά από 100px κύλισης
+        document.body.classList.add('scrolled');
+    } else {
+        document.body.classList.remove('scrolled');
+    }
+});
 
