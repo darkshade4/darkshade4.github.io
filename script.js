f0b7d1d63b65427e1516f7f1c28997e5dd5662dc
zervakisai <zervakisai@gmail.com>
2024-11-05 00:40:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ const navLinks = document.querySelector('.nav-links');
 burger.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
+
+// Parallax Effect
+window.addEventListener('scroll', function() {
+    const offset = window.pageYOffset;
+    const layer1 = document.querySelector('.layer1');
+    const layer2 = document.querySelector('.layer2');
+
+    layer1.style.transform = `translateY(${offset * 0.5}px)`;
+    layer2.style.transform = `translateY(${offset * 0.3}px)`;
+});
